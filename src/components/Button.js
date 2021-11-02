b7366f062e1262d2cf9b3c3d7ead4907bc3a7058
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-11-01 23:37:43 -0700

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+const Button = () => {
+
+    const [ value, setValue ] = useState(0);
+
+    const increaseCount = () => {
+        setValue(prevCount => prevCount + 1);
+    } 
+
+    const decreaseCount = () => {
+        setValue(prevCount => prevCount - 1);
+    }
+
+    return(
+        <div>
+            <h4>Count{value}</h4>
+            <button onClick={increaseCount}>+</button>
+            <button onClick={decreaseCount}>-</button>
+            <button onClick={() => setValue(0)}>reset</button>
+        </div>
+    )
+}
+
+export default Button;
